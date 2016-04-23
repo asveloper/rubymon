c239903e68340432d873cde5c6aaa908150643fc
Arslan Nasir <arslan@mobylogix.com>
2016-04-23 11:01:34 +0300

    Monsters migration updated

--- db/migrate/20160422161633_create_monsters.rb
+++ db/migrate/20160422161633_create_monsters.rb
@@ -2,7 +2,7 @@ class CreateMonsters < ActiveRecord::Migration
   def change
     create_table :monsters do |t|
       t.string :name
-      t.number :power
+      t.integer :power
       t.string :type
 
       t.timestamps null: false
