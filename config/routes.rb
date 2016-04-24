08d0ea15312ac1987723065c1011793c6a3d7a58
Arslan Nasir <arslan@mobylogix.com>
2016-04-24 17:20:05 +0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
   root 'pages#index'
 
   get 'pages/index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :monsters
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
